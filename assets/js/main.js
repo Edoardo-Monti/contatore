beeca216a77729792e4a07662e6b0625baeb9c5f
Edoardo Monti <edoardo.monti.bo@gmail.com>
2024-02-23 13:30:56 +0100

    implementato il set intervall

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 let screen = document.querySelector(".screen")
+let start 
+let soldatino = false
 
 document.getElementById("plus").addEventListener("click", function(){
     
@@ -14,4 +16,30 @@ document.getElementById("minus").addEventListener("click", function(){
     let results = Number(screen.value)
     console.log(results)
 })
+
+
+document.getElementById("start").addEventListener("click", function(){
+    if(!soldatino){
+        start = setInterval(startFunction, 1000);
+        soldatino = true
+    }
+})
+
+document.getElementById("stop").addEventListener("click", function(){
+    stopFunction()
+    soldatino = false
+})
+
+
+function startFunction(){
+    screen.innerHTML++
+    screen.value = screen.innerHTML
+    let results = Number(screen.value)
+    console.log(results)
+}
+
+
+function stopFunction() {
+    clearInterval(start);
+  }
 
